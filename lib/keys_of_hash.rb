586974d6834b06d1bc7675323b422fbf7581f575
Nicholas Dover <dovern42@gmail.com>
2019-02-10 04:47:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,17 +7,6 @@ class Hash
         array.push(key)
       end
     end
-=begin   array = []
-    binding.pry
-    .each do |key,value|
-      if items.include?(value)
-        array.push(key)
-      else
-        array.push(key)
-      end
-    end
-    array
-    # code goes here
-=end
   end
+  array
 end
