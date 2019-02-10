c14e31a741f1dabefc0074814e904b178e4a7d3f
Nicholas Dover <dovern42@gmail.com>
2019-02-10 04:43:52 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*items)
+    self
+=begin   array = []
+    binding.pry
+    .each do |key,value|
+      if items.include?(value)
+        array.push(key)
+      else
+        array.push(key)
+      end
+    end
+    array
     # code goes here
-  end
+=end  end
 end
