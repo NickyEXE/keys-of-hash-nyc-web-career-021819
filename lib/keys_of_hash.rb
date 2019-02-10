137a70720af61330930a51437830c106c66db3e8
Nicholas Dover <dovern42@gmail.com>
2019-02-10 04:48:16 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*items)
     array =[]
     self.each do |key, elements|
-      if items.include?(value)
+      if items.include?(elements)
         array.push(key)
       end
     end
